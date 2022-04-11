d636b1700c0788762d3fd0d61324356577c13420
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2022-04-11 11:39:18 +0200

    Add TweenMax script

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const ball = document.querySelector('.ball')
+const winHeight = window.innerHeight - 150;
+console.log(winHeight);
 
-TweenMax.to('.ball', 5, {bottom: 0} )
+
+TweenMax.to(ball, 3, {y: winHeight,  duration: 2.5, ease: "bounce.out"} )
+TweenMax.to(ball, 2, {backgroundColor: '#1E293B', delay: 4} )
